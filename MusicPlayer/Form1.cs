c3144b7e31d4cb1b1593bc4c4928906fa27a8a4a
Mafari <ahmadfadlan0896@gmail.com>
2023-01-29 23:30:50 +0700

    yes

--- MusicPlayer/Form1.cs
+++ MusicPlayer/Form1.cs
@@ -24,7 +24,7 @@ namespace MusicPlayer
 
         private void button3_Click(object sender, EventArgs e)
         {
-            player.controls.pause()
+            player.controls.pause();
         }
     }
 }
