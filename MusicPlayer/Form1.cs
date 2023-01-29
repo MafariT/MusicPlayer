4961ec324e037d72b18f819abbc0f405ac57e38f
Mafari <ahmadfadlan0896@gmail.com>
2023-01-29 23:26:08 +0700

    Add project files.

---
+++ MusicPlayer/Form1.cs
@@ -0,0 +1,30 @@
+using WMPLib;
+
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.URL = @"C:\Users\Evaluation Software\source\repos\MusicPlayer\MusicPlayer\music tes\Bcalm - Elements - rose fields.mp3";
+            player.controls.play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.controls.stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            player.controls.pause()
+        }
+    }
+}
