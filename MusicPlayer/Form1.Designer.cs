92b3ce137630ba06d1d7f2067c638b17f4e93642
Mafari <ahmadfadlan0896@gmail.com>
2023-01-30 02:03:48 +0700

    added timer still dank

--- MusicPlayer/Form1.Designer.cs
+++ MusicPlayer/Form1.Designer.cs
@@ -28,17 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.currentlyPlay = new System.Windows.Forms.Label();
+            this.MusicTimer = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(93, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(226, 23);
+            this.button1.Size = new System.Drawing.Size(238, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "PLAY";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,7 +48,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(325, 12);
+            this.button2.Location = new System.Drawing.Point(337, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -72,15 +74,26 @@
             this.currentlyPlay.Size = new System.Drawing.Size(0, 15);
             this.currentlyPlay.TabIndex = 3;
             // 
+            // MusicTimer
+            // 
+            this.MusicTimer.AutoSize = true;
+            this.MusicTimer.Location = new System.Drawing.Point(188, 76);
+            this.MusicTimer.Name = "MusicTimer";
+            this.MusicTimer.Size = new System.Drawing.Size(37, 15);
+            this.MusicTimer.TabIndex = 4;
+            this.MusicTimer.Text = "Timer";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 72);
+            this.ClientSize = new System.Drawing.Size(424, 100);
+            this.Controls.Add(this.MusicTimer);
             this.Controls.Add(this.currentlyPlay);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Music Player";
             this.ResumeLayout(false);
@@ -94,5 +107,6 @@
         private Button button2;
         private Button button3;
         private Label currentlyPlay;
+        private Label MusicTimer;
     }
 }
--- MusicPlayer/Form1.cs
+++ MusicPlayer/Form1.cs
@@ -1,27 +1,40 @@
+using System;
+using System.Windows.Forms;
 using WMPLib;
 
 
 namespace MusicPlayer
 {
     public partial class Form1 : Form
-    {
+    {   
         WindowsMediaPlayer player = new WindowsMediaPlayer();
         OpenFileDialog openFileDialog = new OpenFileDialog();
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();  
   
         public Form1()
         {
             InitializeComponent();
-            openFileDialog.Filter = "Music Files|*.mp3;*.wma;*.wav";
+            openFileDialog.Filter = "Music Files|*.mp3;*.wma;*.wav"; 
             openFileDialog.Title = "Select a Music File";
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(UpdateTime);
         }
+        // update the timer
+        private void UpdateTime(object sender, EventArgs e)
+        {
+            MusicTimer.Text = player.controls.currentPositionString;
+        }
+
         //play button
         private void button1_Click(object sender, EventArgs e)
         {
+            // pop out the explorer
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 player.URL = openFileDialog.FileName;
                 player.controls.play();
                 currentlyPlay.Text = openFileDialog.SafeFileName;
+                timer.Start();
             }
         }
         //stop button
@@ -29,10 +42,12 @@ namespace MusicPlayer
         {
             player.controls.stop();
             currentlyPlay.Text = "";
+            MusicTimer.Text = "";
         }
         //pause button
         private void button3_Click(object sender, EventArgs e)
         {
+            // this check if the music is playing or not
             if (player.playState == WMPPlayState.wmppsPlaying)
             {
                 player.controls.pause();
