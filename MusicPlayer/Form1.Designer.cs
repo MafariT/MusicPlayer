badc494e7ccb7ad481f6f63b20e7639f73470505
Mafari <ahmadfadlan0896@gmail.com>
2023-01-31 00:05:41 +0700

    Adding next/prev and playlist feature

--- MusicPlayer/Form1.Designer.cs
+++ MusicPlayer/Form1.Designer.cs
@@ -35,14 +35,19 @@
             this.currentlyPlay = new System.Windows.Forms.Label();
             this.MusicTimer = new System.Windows.Forms.Label();
             this.VolumeBar = new System.Windows.Forms.TrackBar();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.MusicGridView = new System.Windows.Forms.DataGridView();
+            this.PreviousButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MusicGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // PlayButton
             // 
-            this.PlayButton.Location = new System.Drawing.Point(93, 12);
+            this.PlayButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.PlayButton.Location = new System.Drawing.Point(293, 12);
             this.PlayButton.Name = "PlayButton";
-            this.PlayButton.Size = new System.Drawing.Size(238, 23);
+            this.PlayButton.Size = new System.Drawing.Size(281, 23);
             this.PlayButton.TabIndex = 0;
             this.PlayButton.Text = "PLAY";
             this.PlayButton.UseVisualStyleBackColor = true;
@@ -50,9 +55,10 @@
             // 
             // StopButton
             // 
-            this.StopButton.Location = new System.Drawing.Point(337, 12);
+            this.StopButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.StopButton.Location = new System.Drawing.Point(580, 12);
             this.StopButton.Name = "StopButton";
-            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.Size = new System.Drawing.Size(100, 23);
             this.StopButton.TabIndex = 1;
             this.StopButton.Text = "STOP";
             this.StopButton.UseVisualStyleBackColor = true;
@@ -60,9 +66,10 @@
             // 
             // PauseButton
             // 
-            this.PauseButton.Location = new System.Drawing.Point(12, 12);
+            this.PauseButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.PauseButton.Location = new System.Drawing.Point(187, 12);
             this.PauseButton.Name = "PauseButton";
-            this.PauseButton.Size = new System.Drawing.Size(75, 23);
+            this.PauseButton.Size = new System.Drawing.Size(100, 23);
             this.PauseButton.TabIndex = 2;
             this.PauseButton.Text = "PAUSE";
             this.PauseButton.UseVisualStyleBackColor = true;
@@ -71,7 +78,7 @@
             // currentlyPlay
             // 
             this.currentlyPlay.AutoSize = true;
-            this.currentlyPlay.Location = new System.Drawing.Point(12, 38);
+            this.currentlyPlay.Location = new System.Drawing.Point(12, 43);
             this.currentlyPlay.Name = "currentlyPlay";
             this.currentlyPlay.Size = new System.Drawing.Size(0, 15);
             this.currentlyPlay.TabIndex = 3;
@@ -79,14 +86,15 @@
             // MusicTimer
             // 
             this.MusicTimer.AutoSize = true;
-            this.MusicTimer.Location = new System.Drawing.Point(12, 54);
+            this.MusicTimer.Location = new System.Drawing.Point(12, 64);
             this.MusicTimer.Name = "MusicTimer";
             this.MusicTimer.Size = new System.Drawing.Size(0, 15);
             this.MusicTimer.TabIndex = 4;
             // 
             // VolumeBar
             // 
-            this.VolumeBar.Location = new System.Drawing.Point(420, 12);
+            this.VolumeBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.VolumeBar.Location = new System.Drawing.Point(810, 12);
             this.VolumeBar.Maximum = 100;
             this.VolumeBar.Name = "VolumeBar";
             this.VolumeBar.Orientation = System.Windows.Forms.Orientation.Vertical;
@@ -94,11 +102,48 @@
             this.VolumeBar.TabIndex = 5;
             this.VolumeBar.Scroll += new System.EventHandler(this.VolumeBar_Scroll);
             // 
+            // NextButton
+            // 
+            this.NextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.NextButton.Location = new System.Drawing.Point(780, 85);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(75, 23);
+            this.NextButton.TabIndex = 7;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // MusicGridView
+            // 
+            this.MusicGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MusicGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.MusicGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MusicGridView.Location = new System.Drawing.Point(12, 114);
+            this.MusicGridView.Name = "MusicGridView";
+            this.MusicGridView.RowTemplate.Height = 25;
+            this.MusicGridView.Size = new System.Drawing.Size(843, 206);
+            this.MusicGridView.TabIndex = 6;
+            // 
+            // PreviousButton
+            // 
+            this.PreviousButton.Location = new System.Drawing.Point(12, 85);
+            this.PreviousButton.Name = "PreviousButton";
+            this.PreviousButton.Size = new System.Drawing.Size(75, 23);
+            this.PreviousButton.TabIndex = 8;
+            this.PreviousButton.Text = "Previous";
+            this.PreviousButton.UseVisualStyleBackColor = true;
+            this.PreviousButton.Click += new System.EventHandler(this.PreviousButton_Click);
+            // 
             // MusicForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(461, 83);
+            this.ClientSize = new System.Drawing.Size(867, 333);
+            this.Controls.Add(this.PreviousButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.MusicGridView);
             this.Controls.Add(this.VolumeBar);
             this.Controls.Add(this.MusicTimer);
             this.Controls.Add(this.currentlyPlay);
@@ -109,6 +154,7 @@
             this.Name = "MusicForm";
             this.Text = "Music Player";
             ((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MusicGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,5 +168,8 @@
         private Label currentlyPlay;
         private Label MusicTimer;
         private TrackBar VolumeBar;
+        private Button NextButton;
+        private DataGridView MusicGridView;
+        private Button PreviousButton;
     }
 }
--- MusicPlayer/Form1.cs
+++ MusicPlayer/Form1.cs
@@ -8,6 +8,9 @@ namespace MusicPlayer
         WindowsMediaPlayer player = new WindowsMediaPlayer();   // Windows Media Player instance
         OpenFileDialog openFileDialog = new OpenFileDialog();   // Open file dialog instance to select music files
         System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();    // Timer instance to keep track of time
+        List<string> playlist = new List<string>(); // List to keep track of playlist
+        int currentSongIndex = 0; // Index of current song being played
+       
 
         public MusicForm()
         {
@@ -16,10 +19,13 @@ namespace MusicPlayer
             // Setting filter for open file dialog to select only music files
             openFileDialog.Filter = "Music Files|*.mp3;*.wma;*.wav"; 
             openFileDialog.Title = "Select a Music File";
+            openFileDialog.Multiselect = true; 
 
             // Setting timer interval to 1000 milliseconds
             timer.Interval = 1000; 
             timer.Tick += new EventHandler(UpdateTime);
+
+            MusicGridView.CellClick += MusicGridView_CellClick;
         }
         // Method to update time on the form
         private void UpdateTime(object sender, EventArgs e)
@@ -41,10 +47,22 @@ namespace MusicPlayer
             // Showing open file dialog to select music file
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                player.URL = openFileDialog.FileName;   // Setting music file to player
-                player.controls.play();     // Playing music
-                currentlyPlay.Text = openFileDialog.SafeFileName;   // Updating currently playing text
-                timer.Start();      // Starting timer
+                // Adding selected music files to playlist
+                playlist.AddRange(openFileDialog.FileNames);
+
+                // Data source of the grid
+                MusicGridView.DataSource = null;
+                MusicGridView.DataSource = playlist.Select(x => new { Song = Path.GetFileName(x) }).ToList(); // Adding musics to grid
+                MusicGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; // Fill the entire DataGrid
+
+                // Checking if playlist is not empty
+                if (playlist.Count > 0)
+                {
+                    player.URL = playlist[0];   // Setting first music file to player
+                    player.controls.play();     // Playing music
+                    currentlyPlay.Text = Path.GetFileName(playlist[0]);   // Updating currently playing text
+                    timer.Start();      // Starting timer
+                }
             }
         }
 
@@ -71,9 +89,53 @@ namespace MusicPlayer
             MusicTimer.Text = "";   // Clearing time text
         }
 
+        // Method to skip music in current playlist
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            // Checking if the playlist is not empty and there is next song in the playlist
+            if (playlist.Count > 0 && currentSongIndex + 1 < playlist.Count)
+            {
+                selectedRowIndex++;   // Incrementing selectedRowIndex index
+                player.URL = playlist[selectedRowIndex];   // Setting next song to player
+                player.controls.play();     // Playing music
+                currentlyPlay.Text = Path.GetFileName(playlist[selectedRowIndex]);   // Updating currently playing text
+                timer.Start();      // Starting timer
+            }
+        }
+        // Method to go back music in current playlist
+        private void PreviousButton_Click(object sender, EventArgs e)
+        {
+            // Checking if the playlist is not empty and there is previous song in the playlist
+            if (playlist.Count > 0 && currentSongIndex - 1 < playlist.Count)
+            {
+                selectedRowIndex--;   // Incrementing selectedRowIndex index
+                player.URL = playlist[selectedRowIndex];   // Setting next song to player
+                player.controls.play();     // Playing music
+                currentlyPlay.Text = Path.GetFileName(playlist[selectedRowIndex]);   // Updating currently playing text
+                timer.Start();      // Starting timer
+            }
+        }
+
+        // Method to play music when selected cell pressed
+        private int selectedRowIndex;   
+        private void MusicGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedRowIndex = e.RowIndex;  // So that we can use it on different method
+
+            player.URL = playlist[e.RowIndex]; //   Setting music to file to player base on which row pressed
+            player.controls.play();     // Playing Music
+            currentlyPlay.Text = Path.GetFileName(playlist[e.RowIndex]);    // Updating currently playing text
+            if (MusicTimer.Text == "")  // Starting timer if the timer stop
+            {
+                timer.Start();
+            }
+        }
+
+        // Method to control volume
         private void VolumeBar_Scroll(object sender, EventArgs e)
         {
             player.settings.volume = VolumeBar.Value;   // Updating volume value
         }
+
     }
 }
